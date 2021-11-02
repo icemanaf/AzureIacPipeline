264baf9b17b0c320a00afd2fad70e8bab9a9eac0
unknown <dilantha.ariyapala@googlemail.com>
2021-11-02 13:29:43 +0000

    removed acces key from main.tf

--- main.tf
+++ main.tf
@@ -4,7 +4,7 @@ terraform{
         resource_group_name = "rgInfrastructure"
         storage_account_name = "infrastorageaccount1111"
         container_name = "tfstate"
-        key = "b/3nlL+gJltTdOndWKarV6jTmjTGD98fnzMBoHXxBof/hNloe5F5bakzDRQjRE1uTi9fU5eN7Rq8hnOzXTxBYg=="
+        key = "infra.tfstate"
     }
     
 }
